e994df74054575d0eb07ccc40542b287ddc5a679
post-punk <vlado544@yahoo.com>
2019-03-12 16:09:35 +0100

    done

--- src/components/ProductListOne.vue
+++ src/components/ProductListOne.vue
@@ -2,20 +2,34 @@
   <div id="product-list-one">
     <h2>Product list One</h2>
     <ul>
-      <li v-for="product in products">
+      <li v-for="product in saleProducts">
         <span class="name">{{product.name}}</span>
         <span class="price">${{product.price}}</span>
       </li>
     </ul>
+    <button v-on:click="reducePrice(4)">Reduce Price</button>
   </div>
 </template>
 
 <script>
+import { mapActions } from "vuex";
+import { mapGetters } from "vuex";
+
 export default {
   computed: {
     products() {
       return this.$store.state.products;
-    }
+    },
+    ...mapGetters([
+      'saleProducts',
+
+    ])
+  },
+  methods: {
+    ...mapActions([
+      'reducePrice',
+      
+    ])
   },
   data() {
     return {};
@@ -24,24 +38,24 @@ export default {
 </script>
 
 <style scoped>
-#product-list-one{
-    background: #FFF8B1;
-    box-shadow: 1px 2px 3px rgba(0,0,0,0.2);
-    margin-bottom: 30px;
-    padding: 10px 20px;
+#product-list-one {
+  background: #fff8b1;
+  box-shadow: 1px 2px 3px rgba(0, 0, 0, 0.2);
+  margin-bottom: 30px;
+  padding: 10px 20px;
 }
-#product-list-one ul{
-    padding: 0;
+#product-list-one ul {
+  padding: 0;
 }
-#product-list-one li{
-    display: inline-block;
-    margin-right: 10px;
-    margin-top: 10px;
-    padding: 20px;
-    background: rgba(255,255,255,0.7);
+#product-list-one li {
+  display: inline-block;
+  margin-right: 10px;
+  margin-top: 10px;
+  padding: 20px;
+  background: rgba(255, 255, 255, 0.7);
 }
-.price{
-    font-weight: bold;
-    color: #E8800C;
+.price {
+  font-weight: bold;
+  color: #e8800c;
 }
 </style>
--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,40 @@ import Vuex from 'Vuex';
 
 Vue.use(Vuex);
 
-export const store = new Vuex.Store ({
+export const store = new Vuex.Store({
     state: {
         products: [
-            {name: 'banana skin', price: 20},
-            {name: 'shiny star', price: 40},
-            {name: 'green shells', price: 60},
-            {name: 'red shells', price: 80}
-          ]
+            { name: 'banana skin', price: 20 },
+            { name: 'shiny star', price: 40 },
+            { name: 'green shells', price: 60 },
+            { name: 'red shells', price: 80 }
+        ]
+    },
+    getters: {
+        saleProducts: state => {
+
+            var saleProducts = state.products.map(product => {
+                return {
+                    name: "**" + product.name + "**",
+                    price: product.price / 2
+                };
+            });
+            return saleProducts;
+
+        }
+    },
+    mutations: {
+        reducePrice: (state, payload) => {
+            state.products.forEach(function (product) {
+                return product.price -= payload
+            });
+        }
+    },
+    actions: {
+        reducePrice: (context, payload) => {
+            setTimeout(function(){
+                context.commit('reducePrice', payload)
+            }, 300)
+        }
     }
 })
